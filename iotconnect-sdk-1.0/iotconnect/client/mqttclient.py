c57f3b05a1357a479bc3fdf8bb0dd7386eac5384
VPutys <vputys@witekio.com>
2023-07-17 18:14:48 +0100

    jsonlib checking for python version before import

--- iotconnect-sdk-1.0/iotconnect/client/mqttclient.py
+++ iotconnect-sdk-1.0/iotconnect/client/mqttclient.py
@@ -3,6 +3,8 @@ import os
 import ssl as ssl
 import paho.mqtt.client as mqtt
 import json
+if ('linux' in sys.platform) and (sys.version_info >=(3,5)) and (sys.version_info < (3,7)):
+    import jsonlib
 import time
 from iotconnect.IoTConnectSDKException import IoTConnectSDKException
 
